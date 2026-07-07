5fb77bb6484a02a50a60c92c8d7489648a36a348
Shaurya Pratap Singh <singhshauryapratap636@gmail.com>
2026-07-07 14:51:06 +0530

    render 4

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,33 +1,9 @@
 import axios from "axios";
 
-const getBaseUrl = () => {
-  const configuredBase =
-    process.env.REACT_APP_BACKEND_URL ||
-    process.env.REACT_APP_API_URL ||
-    process.env.REACT_APP_BASE_URL ||
-    "";
-
-  if (configuredBase) {
-    if (configuredBase.startsWith("http://") || configuredBase.startsWith("https://")) {
-      return configuredBase.endsWith("/api")
-        ? configuredBase
-        : `${configuredBase.replace(/\/$/, "")}/api`;
-    }
-
-    return configuredBase.startsWith("/")
-      ? `${configuredBase.replace(/\/$/, "")}/api`
-      : `/${configuredBase.replace(/\/$/, "")}/api`;
-  }
-
-  if (typeof window !== "undefined") {
-    return `${window.location.origin}/api`;
-  }
-
-  return "http://localhost:5000/api";
-};
-
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000/api",
 });
 
 export default API;
